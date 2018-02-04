42475a58026cdac96bdf4a21a71894bf29ab0c9d
StanleyE <erica.stanley@live.ca>
2018-02-04 11:48:12 -0500

    Add files via upload

---
+++ assignment-1.js
@@ -0,0 +1,16 @@
+// BrainStation; Assignement 1
+// Part A
+
+var numbers = [];
+    for (i=0; i < 10; i++){
+        numbers.push(i);
+    }
+// console.log(numbers); to see that the numbers went into the array
+console.log(numbers[0]);
+console.log(numbers[9]);
+
+
+//Part B
+var car = { };
+car.color = "blank";
+console.log(car);
